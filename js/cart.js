e9f55c54e2118e69adfe29235e57345528ce1a55
adminov <batishnazik@gmail.com>
2021-11-25 18:37:05 +0500

    'sendFormFetch'

--- js/cart.js
+++ js/cart.js
@@ -2,7 +2,9 @@ const cart = () => {
     const modalCart = document.getElementById('modal-cart');
     const longGoodsList = document.querySelector('.long-goods-list');
     const cartTableGoods = document.querySelector('.cart-table__goods');
-
+    const modalForm = document.querySelector('.modal-form');
+    const cardTableTotal = document.querySelector('.card-table__total');
+    const forPrice = document.querySelector('.forPrice');
 
     const deleteCartItem = (id) => {
         const cart = JSON.parse(localStorage.getItem('cart'));
@@ -43,34 +45,37 @@ const cart = () => {
     //формирование товара в корзине
     const renderCartGoods = (cartArray) => {
         cartTableGoods.innerHTML = '';
+        cardTableTotal.innerHTML = '';
+
         cartArray.forEach((item) => {
             const {name, price, count} = item;
             const  tr = document.createElement('tr');
-
+            tr.classList.add('forPrice');
             tr.innerHTML = `
                 <td>${name}</td>
                 <td>${price}$</td>
                 <td><button class="cart-btn-minus"">-</button></td>
                 <td>${count}</td>
                 <td><button class="cart-btn-plus"">+</button></td>
-                <td>${+price * +count}$</td>
+                <td class="price">${+price * +count}$</td>
                 <td><button class="cart-btn-delete"">x</button></td>
             `;
 
             cartTableGoods.append(tr);
 
             tr.addEventListener('click', (event) => {
-                console.log(event.target);
+
                 let target = event.target;
                 if (target.classList.contains('cart-btn-minus')) {
                     minusCartItem(item.id);
-                    console.log(item.id);
                 } else if (target.classList.contains('cart-btn-plus')) {
                     plusCartItem(item.id);
                 } else if (target.classList.contains('cart-btn-delete')) {
                     deleteCartItem(item.id);
                 }
-            })
+            });
+
+            TotalPriceChange()
         })
     };
 
@@ -125,6 +130,43 @@ const cart = () => {
             }
         })
     }
+
+    const TotalPriceChange = () => {
+        //Total Price
+        const forPrice = cartTableGoods.querySelectorAll('.price');
+        let tmp = 0;
+        forPrice.forEach(item => {
+            cardTableTotal.innerHTML = `${tmp += +item.textContent.replace('$', '')}$`;
+        });
+    };
+
+    //отправка данных из корзины
+    const sentForm = () => {
+        const cartArray = localStorage.getItem('cart') ?
+            JSON.parse(localStorage.getItem('cart')) : [];
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify({
+                cart: cartArray,
+                name: '',
+                phone: '',
+                totalPrice: TotalPriceChange()
+            })
+        }).then(() => {
+            modalCart.style.display = '';
+            localStorage.removeItem('cart');
+            const nameValue = modalForm.querySelectorAll('.modal-input');
+            nameValue.forEach(item => {
+                item.value = '';
+            });
+        })
+    };
+
+    modalForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        sentForm();
+    });
 };
 
 cart();
